3e00085a33e54747549cad961f0e37483ef3df0f
Cliff-Ezra <cliffezra212@gmail.com>
2024-06-13 02:10:51 +0300

    `style:` remove model component

--- app/page.js
+++ app/page.js
@@ -224,10 +224,12 @@ export default function HomePage() {
       <CallToAction />
 
       <nav className="sm:pt-8 pt-4 px-4 sm:px-12 flex items-center">
-        <div className="pr-3 font-semibold text-gray-500">Chat with</div>
-        <div className="font-semibold text-gray-500 sm:text-center">
-          <Dropdown models={MODELS} selectedModel={model} setModel={setModel} />
+        <div className="pr-3 font-semibold text-gray-500">
+          Chat with Law Guru on Recent & Up-to-date Data 😌
         </div>
+        {/* <div className="font-semibold text-gray-500 sm:text-center">
+          <Dropdown models={MODELS} selectedModel={model} setModel={setModel} />
+        </div> */}
         <div className="flex-grow"></div>
         <div className="justify-end">
           <button
